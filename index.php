5d745d1ed98be97907311e666ea09bbf95208344
Kartheek Midde <car621@gmail.com>
2020-11-02 19:15:13 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <form action="practice.php" method="POST">
+            <input type="text" name="username">
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
